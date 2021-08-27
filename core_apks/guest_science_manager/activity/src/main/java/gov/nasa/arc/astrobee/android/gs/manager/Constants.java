1243695fee1c99a75c07127c6d4eecdd1070dc47
kbrowne15 <kathryn.browne@nasa.gov>
2021-08-27 13:47:00 -0700

    Added a heartbeat to the guest science manager (#19)

--- core_apks/guest_science_manager/activity/src/main/java/gov/nasa/arc/astrobee/android/gs/manager/Constants.java
+++ core_apks/guest_science_manager/activity/src/main/java/gov/nasa/arc/astrobee/android/gs/manager/Constants.java
@@ -27,5 +27,6 @@ final class Constants {
     public static final String TOPIC_GUEST_SCIENCE_MANAGER_ACK = "gs/gs_manager/ack";
     public static final String TOPIC_GUEST_SCIENCE_MANAGER_CONFIG = "gs/gs_manager/config";
     public static final String TOPIC_GUEST_SCIENCE_MANAGER_STATE = "gs/gs_manager/state";
+    public static final String TOPIC_HEARTBEAT = "heartbeat";
     public static final String TOPIC_MANAGEMENT_EXEC_COMMAND = "mgt/executive/command";
 }
